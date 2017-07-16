b25087cdcbcc24919c0e60ce0e5b2bd995c8ae37
Subhrajyoti Sen <subhrajyoti12@gmail.com>
2017-07-16 23:01:44 +0530

    fs: fix su remnants

--- fs/readdir.c
+++ fs/readdir.c
@@ -51,14 +51,6 @@ out:
 }
 EXPORT_SYMBOL(iterate_dir);
 
-static bool hide_name(const char *name, int namlen)
-{
-	if (namlen == 2 && !memcmp(name, "su", 2))
-		if (!su_visible())
-			return true;
-	return false;
-}
-
 /*
  * Traditional linux readdir() handling..
  *
